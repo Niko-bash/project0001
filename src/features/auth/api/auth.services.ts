38adf240584e0ea576d9a687abc042a8c0ff1d8f
Niko-bash <webproject96@mail.ru>
2026-06-05 18:50:43 +0300

    4/feat-auth-register-user

--- src/features/auth/api/auth.services.ts
+++ src/features/auth/api/auth.services.ts
@@ -1,11 +1,52 @@
-import type { AuthUser } from './type'
+import type { AuthUser, CreateUser, User } from './type'
 
 export const AuthServices = {
 	getSessionCookie: () => {},
 	setSessionCookie: () => {},
 	getSession: () => {},
 	SingUp: async (data: AuthUser): Promise<unknown> => {
-		const user = await fetch('')
+		const response = await fetch(`/api/user?email=${data.email}`, {
+			method: 'GET'
+		})
+
+		if (!response.ok) {
+			console.error('BadRequest')
+			return
+		}
+
+		const users: User[] = await response.json()
+		const user = users[0]
+
+		if (user?.email === data.email) {
+			console.error('this user is already registered')
+			return
+		}
+
+		const createDataUser: CreateUser = {
+			...data,
+			role: 'user',
+			name: data.email
+		}
+
+		const createUser = await fetch('/api/user', {
+			method: 'POST',
+			body: JSON.stringify(createDataUser)
+		})
+
+		if (!createUser.ok) {
+			console.log('bad registr')
+			return
+		}
+
+		const thisUser: User = await createUser.json()
+
+		const { id, ...userData } = thisUser
+
+		return {
+			status: 200,
+			success: true,
+			data: userData
+		}
 	},
 	SingIn: () => {},
 	logout: () => {}
