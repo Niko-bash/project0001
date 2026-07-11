dea5ba47bdc06ef298bacfe5b251b33a7b2ba7e4
Niko-bash <webproject96@mail.ru>
2026-07-12 01:45:23 +0300

    28/redesign-switch-courses-services

--- src/features/courses/api/courses.services.ts
+++ src/features/courses/api/courses.services.ts
@@ -1,8 +1,27 @@
-// export const CoursesServices = {
-// 	async add(
-// 		coursesId: string,
-// 		userId: string
-// 	): Promise<ApiResponse<CoursesType>> {
-// 		const response = await fetch('/api/')
-// 	}
-// }
+import type { SearchType } from '@/pages/courses.page'
+import type { InfinityCoursesType } from '../ui/card'
+
+export const CoursesServices = {
+	async getCourses(
+		query: SearchType,
+		page: number,
+		signal?: AbortSignal
+	): Promise<InfinityCoursesType> {
+		const queryParams = new URLSearchParams({
+			'name:contains': query.title || '',
+			_page: String(page),
+			_per_page: query.per_page || '10',
+			_sort: query.sort || '-rating'
+		}).toString()
+
+		const response = await fetch(`/api/courses?` + queryParams, {
+			method: 'GET',
+			signal: signal
+		})
+		if (!response.ok) {
+			throw new Error()
+		}
+		const data: InfinityCoursesType = await response.json()
+		return data
+	}
+}
